6f6a30d684accece8cde53b2da1c6bf975347b7b
amarmandal <amarmandal2073@gmail.com>
2019-12-26 09:07:15 +0545

    File update

---
+++ LinkedList/lStackL.h
@@ -0,0 +1,24 @@
+#ifndef LSTACK_H
+#define LSTACK_H
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+class StackL
+{
+private:
+    Node *top;
+
+public:
+    StackL();
+    ~StackL();
+    void push(int);
+    void pop();
+    void display();
+};
+
+#endif
